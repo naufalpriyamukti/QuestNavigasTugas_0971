b9bb3e09e60d849f9217e0a7f619396bb467e397
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:33:21 +0700

    Tambah layout dasar

--- app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
@@ -1,6 +1,15 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TampilData(
@@ -12,5 +21,16 @@ fun TampilData(
     onFormulirClick: () -> Unit
 )
 {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEDE7F6))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+
+    }
 
 }
