03480d47dedfc24c660040db067c414779a91f40
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 20:58:17 +0700

    Konfigurasi NavHost dan tujuan awal

--- app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
@@ -1,8 +1,11 @@
 package com.example.questnavigastugas_097
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -16,7 +19,11 @@ fun Prak6App(
 )
 {
     Scaffold { innerPadding ->
-
+        NavHost(
+            navController = navController,
+            startDestination = com.example.prak6.Navigasi.HomeDepan.name,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 
 }
