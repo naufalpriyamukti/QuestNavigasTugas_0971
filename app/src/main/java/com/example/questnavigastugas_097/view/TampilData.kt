8ae8ddfa100867b16be33a9a7cbe04aa22ae283f
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:37:09 +0700

    Tambah header daftar peserta

--- app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TampilData(
@@ -28,9 +31,25 @@ fun TampilData(
             .padding(16.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF9C27B0))
+                .padding(vertical = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id= R.string.list),
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
 
     }
+
+
 
 }
