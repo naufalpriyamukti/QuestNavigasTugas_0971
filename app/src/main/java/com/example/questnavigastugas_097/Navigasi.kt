249923bf378e8c7408c6855e81ed074f23b89d05
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 20:57:06 +0700

    Menambahkan scaffold

--- app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas_097
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -13,3 +14,9 @@ enum class Navigasi {
 fun Prak6App(
     navController: NavHostController = rememberNavController()
 )
+{
+    Scaffold { innerPadding ->
+
+    }
+
+}
