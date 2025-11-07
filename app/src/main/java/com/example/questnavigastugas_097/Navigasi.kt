149678b1f13946a5f9787bebd8fd0476cc94e42f
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 20:55:00 +0700

    Menambahkan enum untuk rute navigasi

--- app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_097/Navigasi.kt
@@ -1,2 +1,15 @@
 package com.example.questnavigastugas_097
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    HomeDepan,
+    Formulir,
+    TampilData
+}
+@Composable
+fun Prak6App(
+    navController: NavHostController = rememberNavController()
+)
